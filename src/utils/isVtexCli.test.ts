4c4cf58e73bd1d885be37d2a66f70b74ce8db1de
marlon Felipe Passos <marlonpassos@agenciam3.com>
2022-09-30 04:20:15 -0300

    test(isVtexvCli): funcionalidades basicas

--- src/utils/isVtexCli.test.ts
+++ src/utils/isVtexCli.test.ts
@@ -1,7 +1,23 @@
 import { isVtexCli } from './isVtexCli'
 import errorMessages from '../constants/errorMessages'
+import * as cli  from './cli'
+
 describe('unit - isVtexCli', () => {
-  it("Se não existir deve retornar um erro de errorMessages", async () =>  { 
+  it("Se não existir deve retornar um erro de errorMessages", async () => {
+
+    jest
+      .spyOn(cli, "cli")
+      .mockResolvedValue({ stderr: "", stdout: "error", error: null });
+
+
     await expect(() => isVtexCli()).rejects.toThrowError(errorMessages.vtexNotFound)
   })
+
+  it("não deve fazer deve dar erro existir algum texto da vtex", async () => {
+    jest
+      .spyOn(cli, "cli")
+      .mockResolvedValue({ stderr: "", stdout: "vtex", error: null });
+
+    expect(await isVtexCli()).toBeUndefined()
+   });
 });
