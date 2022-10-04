4bcf7f8d26a17ad68fe04419661210149aef60d1
marlon Felipe Passos <marlonpassos@agenciam3.com>
2022-10-03 22:36:34 -0300

    refactor: facilita o mock

--- src/utils/isVtexCli.test.ts
+++ src/utils/isVtexCli.test.ts
@@ -1,22 +1,18 @@
 import { isVtexCli } from './isVtexCli'
 import errorMessages from '../constants/errorMessages'
-import * as cli  from './cli'
+import * as cli from './cli'
+
+const mockCli = jest.spyOn(cli, "cli")
 
 describe('unit - isVtexCli', () => {
   it("Se não existir deve retornar um erro de errorMessages", async () => {
-
-    jest
-      .spyOn(cli, "cli")
-      .mockResolvedValue({ stderr: "", stdout: "error", error: null });
-
+    mockCli.mockResolvedValue({ stderr: "", stdout: "error", error: null });
 
     await expect(() => isVtexCli()).rejects.toThrowError(errorMessages.vtexNotFound)
   })
 
   it("não deve fazer deve dar erro existir algum texto da vtex", async () => {
-    jest
-      .spyOn(cli, "cli")
-      .mockResolvedValue({ stderr: "", stdout: "vtex", error: null });
+    mockCli.mockResolvedValue({ stderr: "", stdout: "vtex", error: null });
 
     expect(await isVtexCli()).toBeUndefined()
    });
