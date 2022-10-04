eab6f97806c57e4a46a2e32da576025bbe766b5e
marlon Felipe Passos <marlonpassos@agenciam3.com>
2022-10-04 01:38:16 -0300

    todos

--- src/utils/CreateComponent.ts
+++ src/utils/CreateComponent.ts
@@ -29,10 +29,14 @@ export class CreateComponent {
     this.name = name;
     this.path = path;
     this.nomeEmMaisculo = name.charAt(0).toUpperCase() + name.slice(1);
+
+    // todo: verifica se pessoa tem o diretorio components
     this.pathComponent = `${this.path}/react/components/${this.nomeEmMaisculo}`;
   }
   
   public async create() {
+    // todo: envolver isso em um try catch
+
     await this.createFolder();
     await this.createFileInsideComponent()
     await this.arquivoExportacaoVtex();
@@ -41,11 +45,15 @@ export class CreateComponent {
 
   async createFileInsideComponent() {
     const path = `${this.pathComponent}/index.tsx`;
+    // todo: lidar com erros
+
+    // todo: extrair isso para uma função separada
     await fs.writeFile(path, TEMPLATE.replace(/componentName/g, this.nomeEmMaisculo));
   }
 
   async createFolder() {
     await fs.mkdir(this.pathComponent);
+    //todo criar uma mensagem caso ocorra algum erros ao criar a pasta
   }
 
   async arquivoExportacaoVtex() { 
@@ -57,8 +65,10 @@ export class CreateComponent {
   async adiconaConfiguracaoNoInterfacejson() { 
     const path = `${this.path}/store/interfaces.json`;
     const interfaceJson = await fs.readFile(path, "utf-8");
+    // todo: lidar caso não ache esse arquivo
     const interfaceJsonParse = JSON.parse(interfaceJson);
-
+    
+    // todo: Verifica se já existe essa configuração e informar a pessoa que estamos sobrescrevendo
     const data = {
       ...interfaceJsonParse,
       ...formataConfiguraInterface(this.nomeEmMaisculo)
